253709f1dd52bc0cf0ee27643ed5b56321e6c815
Lianna Asatryan <111121016+LiannaAsatryan@users.noreply.github.com>
2022-12-16 14:04:38 +0400

    Update Shareholders.sol

--- Shareholders.sol
+++ Shareholders.sol
@@ -3,81 +3,47 @@
 pragma solidity 0.8.15;
 
 contract Shareholders {
-    address payable public owner;
-
+   
     constructor(){
         owner = payable(msg.sender);
     }
 
-    modifier onlyOwner{
-        require(msg.sender == owner, "This operation can do only the owner!");
-        _;
-    }
-
-    struct Shareholder {
-        address payable shAddress;
-        uint8 persantage; //0-100%
-    }
-    //array of shareholders
-    Shareholder[] shareholders;
-
-    int sumOfAllPersantages;
-
-    /** @dev checks whether everything is okay with the parsantages of the shareholders*/
-    function checkPersantages() private view {
-       require(sumOfAllPersantages > 0 && sumOfAllPersantages <= 100, "something is wrong with the persantages!");
-    }
-
-    /** @dev checks whether the given array contains the given address
-        @param _array the given array
-        @param _address the given address
-        @return a tuple value - bool(contains or not), int(the index of the element if contains. It's -1 if doesnt contain) */
-    function contains(Shareholder[] memory _array, address _address) private pure returns(bool, int) {
-        for(uint i = 0; i < _array.length; i++) {
-            if(_array[i].shAddress == _address) {
-                return (true, int(i));
-            }
+     /** @dev receives the money and splits to the shateholders with corresponding percantages*/
+    receive() external payable {
+        for(uint i = 0; i < shareholders.length; i++) {
+            uint amount = (msg.value * addressToPercantage[shareholders[i]]) / 100;
+            shareholders[i].transfer(amount);
         }
-        return (false, -1);
     }
-    
+
+    address payable owner;
+    mapping(address => uint8) addressToPercantage;
+    address payable[] shareholders;
+    uint8 sumOfAllPersantages;
+
     /** @dev adds a new shareholder, the function can call only the owner
         @param _address the address of the new shareholder
         @param _persantage the percantage of the new shareholder*/
     function addShareholder(address payable _address, uint8 _persantage) public onlyOwner {
         require(_persantage <= 100, "Invalid persantage");
-        bool contain;
-        (contain,) = contains(shareholders, _address);
-        require(!contain, "This person is already a shareholder");
-        Shareholder memory newShareholder = Shareholder(_address, _persantage);
-        shareholders.push(newShareholder);
-        sumOfAllPersantages += int8(_persantage); 
-        checkPersantages();
+        require(addressToPercantage[_address] == 0, "This person is already a shareholder");
+        addressToPercantage[_address] = _persantage;
+        shareholders.push(_address);
+        sumOfAllPersantages += _persantage; 
+        require(sumOfAllPersantages <= 100, "somethng is wrong with percantages");
     }
     
-     /** @dev removes the shareholder, the function can call only the owner
+    /** @dev removes the shareholder, the function can call only the owner
         @param _address the address of the new shareholder*/
     function removeShareholder(address _address) public onlyOwner {
-        bool contain;
-        int index;
-        (contain, index) = contains(shareholders, _address);
-        require(contain, "This shareholder doesn't exist");
-        uint8 percantage = shareholders[uint(index)].persantage;
-        Shareholder memory temp = shareholders[uint(index)];
-        shareholders[uint(index)] = shareholders[shareholders.length - 1];
-        shareholders[shareholders.length - 1] = temp;
-        shareholders.pop();
-        sumOfAllPersantages -= int8(percantage);
-        checkPersantages();
+        require(addressToPercantage[_address] != 0, "This shareholder doesn't exist");
+        uint8 percantage = addressToPercantage[_address];
+        addressToPercantage[_address] = 0;
+        sumOfAllPersantages -= percantage;
     }  
 
-    
-   /** @dev receives the money and splits to the shateholders with corresponding percantages*/
-    receive() external payable {
-        for(uint i = 0; i < shareholders.length; i++) {
-            uint amount = (msg.value * uint(shareholders[i].persantage)) / 100;
-            shareholders[i].shAddress.transfer(amount);
-        }
+    modifier onlyOwner{
+        require(msg.sender == owner, "This operation can do only the owner!");
+        _;
     }
-
 }
